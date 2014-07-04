1c3f5c56ff77f1d5e69d20888a2a77f03aa986f7
Matthew Forrester <Matthew.Forrseter@pod-point.com>
2014-07-04 18:56:01 +0100

    #2 in c4 done

--- c04exercies.hs
+++ c04exercies.hs
@@ -2,7 +2,11 @@ safeHead :: [a] -> Maybe a
 safeHead (x:xs) = Just x
 safeHead [] = Nothing
 
-splitWith :: (a -> Bool) -> [a] -> [[a]]
-splitWith f xs = 
-  let g x = not (f x)
-  in break g xs
+-- splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith _ [] = []
+splitWith f xs =
+    let g x = not (f x)
+        (pre,suf) = break f xs
+    in pre : case suf of
+        "" -> []
+        suf -> splitWith f (tail suf)
