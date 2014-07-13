fb8d0eb43ad71f16b4a5894c9cf88bd9701a3353
Matthew Forrester <github@speechmarks.com>
2014-07-13 12:58:00 +0100

    p98 questions done

--- c04exercies.hs
+++ c04exercies.hs
@@ -1,4 +1,4 @@
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, isDigit)
 
 safeHead :: [a] -> Maybe a
 safeHead (x:xs) = Just x
@@ -68,3 +68,81 @@ myOr pred (x:xs) =
     then True
     else myOr pred xs
 
+loop2 :: String -> Int
+loop2 str = 
+    let f acc cur = (acc * 10) + digitToInt cur
+    in
+        foldl f 0 str
+
+asInt_fold :: String -> Int
+asInt_fold ('-':str) = -1 * asInt_fold str
+asInt_fold str =
+    let f acc cur = (acc * 10) + digitToInt cur
+    in
+        foldl f 0 str
+
+--  type ErrorMessage = String
+--
+-- asInt_error_foldr :: Int -> Char -> Either ErrorMessage Int
+-- asInt_error_foldr acc cur | isDigit cur = acc + (digitToInt cur * 10)
+-- asInt_error_foldr acc cur | cur == '-' = acc * (-1)
+-- asInt_error_foldr acc cur =  "Non number found '" ++ [cur] ++ "'"
+--
+-- asInt_error :: String -> Either ErrorMessage Int
+-- asInt_error str =
+--     foldr asInt_error_foldr 0 str
+
+myConcatFr :: [[a]] -> [a]
+myConcatFr xs = 
+    foldr (++) [] xs
+
+myTakeWhileRec :: (a -> Bool) -> [a] -> [a]
+myTakeWhileRec f (x:[]) = x:[]
+myTakeWhileRec f (x:xs) | f x == True = x:(myTakeWhileRec f xs)
+myTakeWhileRec _ _ = []
+
+myTakeWhileFoldr :: (a -> Bool) -> [a] -> [a]
+myTakeWhileFoldr pred xs =
+    foldr stepr [] xs
+    where stepr x acc | pred x    = x:acc
+                      | otherwise = []
+
+
+myAnyFoldr :: (a -> Bool) -> [a] -> Bool
+myAnyFoldr pred xs = foldr stepr False xs
+    where stepr x acc | acc == True    = True
+                      | pred x == True = True
+                      | otherwise      = False
+
+
+myGroupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+myGroupBy pred xs = foldl step [] xs
+    where step [] x                             = [[x]]
+          step acc x | pred x (last (head acc)) = (init acc) ++ [ last acc ++ [x] ]
+                     | otherwise                = acc ++ [[x]]
+
+
+-- myCycle :: [a] -> [a]
+-- myCycle xs = foldl step [] xs
+--     where step acc y = acc ++ [xs !! (length acc)]
+
+myWordsL :: String -> [String]
+myWordsL str = foldl step [] str
+    where
+        step [] c                     = [[c]]
+        step acc ' ' | last acc == "" = acc
+        step acc ' '                  = acc ++ [""]
+        step acc c                    = init acc ++ [(last acc) ++ [c]]
+
+myWordsR :: String -> [String]
+myWordsR str = foldr step [] str
+    where
+        step c []                     = [[c]]
+        step ' ' acc | head acc == "" = acc
+        step ' ' acc                  = "" : acc
+        step c   acc                  = ([c] ++ head acc) : tail acc
+
+
+myUnlines :: [String] -> String
+myUnlines xs = foldl step [] xs
+    where step acc x = acc ++ x ++ "\n"
