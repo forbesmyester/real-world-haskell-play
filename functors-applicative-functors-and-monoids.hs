153209212935306b1e8a8afc720919aec6052b19
Matthew Forrester <matthew.forrester@speechmarks.com>
2015-04-17 17:30:24 +0100

    chapter 12 complete...

--- functors-applicative-functors-and-monoids.hs
+++ functors-applicative-functors-and-monoids.hs
@@ -17,3 +17,37 @@
 -- instance Functor CMaybe where
 --     fmap _ CNothing = CNothing
 --     fmap f (CJust counter x) = CJust (counter+1) (f x) 
+--
+import Control.Applicative
+import Control.Monad
+
+-- Just "hello" >>= (\x -> Just $ head x)
+wopwop :: Maybe String -> Maybe Char
+wopwop str = do
+        (x:xs) <- str
+        Just x
+
+
+-- [3,4,5] >>= (\x -> [x, -x])
+-- [3,4,5] >>= (\x -> 1)
+-- [3,4,5] >>= (\x -> if (x == 4) then [] else [x])
+
+type KnightPos = (Int,Int)
+
+knightMove :: KnightPos -> [KnightPos]
+knightMove (x,y) = do
+        (x', y') <- [(x+2,y+1),(x+2,y-1),(x-2,y+1),(x-2,y-1)
+                  ,(x+1,y+2),(x+1,y-2),(x-1,y+2),(x-1,y-2)]
+        guard (x' `elem` [1..8] && y' `elem` [1..8])
+        return (x',y')
+
+firstKnightMove3Times :: KnightPos -> [KnightPos]
+firstKnightMove3Times km = do
+        first <- knightMove km
+        second <- knightMove first
+        knightMove second
+
+secondKnightMove3Times :: KnightPos -> [KnightPos]
+secondKnightMove3Times km = return km >>= knightMove >>= knightMove >>= knightMove
+
+
